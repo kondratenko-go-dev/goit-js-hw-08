35652a43dc46932fdeca7b823c4cb0f196aa1b4c
kondratenko-go-dev <kondratenko.aleksandr.go.dev@gmail.com>
2026-04-16 20:05:21 +0300

    update task-8

--- js/gallery.js
+++ js/gallery.js
@@ -70,13 +70,15 @@ const markup = images
   .map(
     ({ preview, original, description }) =>
       `<li class="gallery__item">
-      <a class="gallery__link" href="${original}">
+         <a class="gallery__link" href="${original}">
         <img
           class="gallery__image"
           src="${preview}"
           data-source="${original}"
           alt="${description}"
         />
+        </a>
+        </li>
     `
   )
   .join('');
